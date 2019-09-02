17c268d7f5f1f1fe695116f7759c1110849b677f
kad15 <kader.beldjilali@free.fr>
2019-09-02 02:01:11 +0200

    rapport PFE

--- kad_maj_git.sh
+++ kad_maj_git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull origin master 1>/dev/null 2>&1
 git add . 1>/dev/null 2>&1
-git commit -m "master-worker" 1>/dev/null 2>&1
+git commit -m "rapport PFE" 1>/dev/null 2>&1
 git push origin master 1>/dev/null 2>&1
 git push origin master 1>/dev/null 2>&1
 git push origin master | grep "Everything up-to-date"
