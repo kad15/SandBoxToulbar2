9c7046210ec950730f8e001054046640044bdcd0
kad15 <kader.beldjilali@free.fr>
2019-09-05 21:10:51 +0200

    rapport PFE

--- kad_maj_git.sh
+++ kad_maj_git.sh
@@ -5,4 +5,5 @@ git commit -m "rapport PFE" 1>/dev/null 2>&1
 git push origin master 1>/dev/null 2>&1
 git push origin master 1>/dev/null 2>&1
 git push origin master | grep "Everything up-to-date"
+echo "Rapport publié sur GitHub"
 sleep 2s
